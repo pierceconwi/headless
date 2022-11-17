58aa452cb85e323faaa396a8d08686b9b9b97ef3
Pierce Conwi <pierceconwi@gmail.com>
2022-11-17 15:22:29 -0800

    /pages/pmc-pages/[id].js now loads content dynamically from the WP API depending on WP post ID. Adjusted Entry() to scrape out HTML tags before displaying post content.

--- pages/pmc-pages/[id].js
+++ pages/pmc-pages/[id].js
@@ -4,9 +4,9 @@ import styles from '../../styles/Home.module.css';
 import { getAllIds, getData, getSortedList } from '../../lib/data';
 import Layout from '../../components/layout.js';
 
-// create an instance of the getStaticProps() to return data for one person
+// create an instance of the getStaticProps() to return data for one post
 export async function getStaticProps({ params }) {
-    const allData = getSortedList();
+    const allData = await getSortedList();
     const itemData = await getData(params.id);
     return {
         props: {
@@ -18,19 +18,26 @@ export async function getStaticProps({ params }) {
 
 // create instance of getStaticPaths() to report to next.js all possible dynamic urls
 export async function getStaticPaths() {
-    const paths = getAllIds();
+    const paths = await getAllIds();
     return {
         paths,
         fallback: false
     };
 }
 
-// make a React.js component to display all details about a person when a dynamic route matches
-export default function Entry( { itemData, allData } ) {
+// make a React.js component to display all details about a post when a dynamic route matches
+export default function Entry( { itemData } ) {
+    // use regular expression to remove wp post's html tags from wp api's provided content before displaying in app
+    var regex = /(<([^>]+)>)/ig,
+    content = itemData.post_content,
+    cleanedPost = content.replace(regex, "");
+    // return output for app page:
     return (
         <main class="card col-6">
             <div class="card-body">
                 <h5 class="card-title">{itemData.post_title}</h5>
+                <p>{itemData.post_date}</p>
+                <p>{cleanedPost}</p>
 
             </div>
                     <a class="btn btn-primary mt-3" href='../'>Back to Home</a>
